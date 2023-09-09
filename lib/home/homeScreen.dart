c6f7aa0d2635fa3ae3459c51dd449d2109aefb5c
AlaaYounes <alaay725@gmail.com>
2023-09-09 11:58:54 +0300

    home screen added

--- lib/home/homeScreen.dart
+++ lib/home/homeScreen.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:islamy/hadethTab.dart';
+import 'package:islamy/quranTab.dart';
+import 'package:islamy/radioTab.dart';
+import 'package:islamy/tasbeehTab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static String routeName = 'homeScreen';
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(children: [
+      Image.asset(
+        'assets/images/background_light.png',
+        width: double.infinity,
+        height: double.infinity,
+        fit: BoxFit.fill,
+      ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Islami',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Theme.of(context).primaryColor,
+          ),
+          child: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/icon_quran.png'),
+                  ),
+                  label: 'Quran'),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/icon_hadeth.png'),
+                  ),
+                  label: 'Hadeth'),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/icon_sebha.png'),
+                  ),
+                  label: 'Tasbeeh'),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/icon_radio.png'),
+                  ),
+                  label: 'Radio'),
+            ],
+            onTap: (index) {
+              selectedIndex = index;
+              setState(() {});
+            },
+            currentIndex: selectedIndex,
+          ),
+        ),
+        body: screens[selectedIndex],
+      ),
+    ]);
   }
+
+  List<Widget> screens = [
+    QuranTab(),
+    HadethTab(),
+    TasbeehTab(),
+    RadioTab(),
+  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:islamy/home/homeScreen.dart';
+import 'package:islamy/myTheme.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
       },
+      theme: MyTheme.lightTheme,
     );
   }
 }
